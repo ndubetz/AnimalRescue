abbeb738cf7c6236b1aab4c1f9a3899779588038
ndubetz <ndubetz@emich.edu>
2014-10-06 13:49:18 -0400

    Why did this file not update when I clicked commit?

--- TestForGitHub/src/pack/Hello.java
+++ TestForGitHub/src/pack/Hello.java
@@ -1,5 +1,7 @@
 package pack;
 
+import java.util.ArrayList;
+
 public class Hello {
 	public static void main(String args[]){
 		for (int i = 0; i < 6; i++) {
@@ -10,6 +12,18 @@ public class Hello {
 				System.out.println("Beans");			
 			}
 		}
-		System.out.println("");
+		System.out.println("Let's make some donuts!");
+		
+		ArrayList<Donut> donuts = new ArrayList<Donut>();
+		
+		donuts.add(DonutFactory.makeDonut(true, "pink"));
+		donuts.add(DonutFactory.makeDonut(false, "chocolate"));
+		donuts.add(DonutFactory.makeDonut(true, "white"));
+		donuts.add(DonutFactory.makeDonut(false, "no"));
+		donuts.add(DonutFactory.makeDonut(true, "chocolate"));
+		
+		for (Donut donut : donuts) {
+			System.out.println(donut);
+		}
 	}
 }
