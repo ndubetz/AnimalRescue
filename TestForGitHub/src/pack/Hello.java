4e5e4fbb95cdd8455365f61a81be099220caede4
ndubetz <ndubetz@emich.edu>
2014-10-06 11:38:22 -0400

    10/6/2014 	NDUB trying some stuff out.

---
+++ TestForGitHub/src/pack/Hello.java
@@ -0,0 +1,14 @@
+package pack;
+
+public class Hello {
+	public static void main(String args[]){
+		for (int i = 0; i < 10; i++) {
+			if(i == 5){
+				System.out.println("Hwuh");
+			}
+			else{
+				System.out.println("Beans");			
+			}
+		}
+	}
+}
