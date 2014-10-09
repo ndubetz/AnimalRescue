6dbb11109173d92215620d74496711b89fe38b04
NATE DUBETZ <nbdubetz@gmail.com>
2014-10-09 19:21:33 -0400

    Update a cat
    updte a cat

--- NewBeginnings/src/model/Cat.java
+++ NewBeginnings/src/model/Cat.java
@@ -10,13 +10,17 @@ public class Cat{
 	private String hairColor;
 	private GregorianCalendar arrivalDate;
 	private GregorianCalendar departureDate;
+	private String id;
+	private boolean isFixed;
 	
-	public Cat(String name, int age, String gender, String breed, String hairColor, GregorianCalendar arrivalDate, GregorianCalendar departureDate){
+	public Cat(String name, int age, String gender, String breed, String hairColor, String id, boolean isFixed, GregorianCalendar arrivalDate, GregorianCalendar departureDate){
 		this.name = name;
 		this.age = age;
 		this.gender = gender;
 		this.breed = breed;
 		this.hairColor = hairColor;
+		this.id = id;
+		this.isFixed = isFixed;
 		this.arrivalDate = arrivalDate;
 		this.departureDate = departureDate;
 	}
@@ -42,12 +46,18 @@ public class Cat{
 	}
 
 	public GregorianCalendar getArrivalDate() {
-		System.out.println(this.arrivalDate.getTime());
 		return this.arrivalDate;
 	}
 
 	public GregorianCalendar getExpectedDepartureDate() {
-		System.out.println(this.departureDate.getTime());
 		return this.departureDate;
+	}
+
+	public String getID() {
+		return this.id;
+	}
+
+	public boolean isFixed() {
+		return this.isFixed;
 	}
 }
--- NewBeginningsTest/src/model/CatTest.java
+++ NewBeginningsTest/src/model/CatTest.java
@@ -15,14 +15,20 @@ public class CatTest {
 		String expectedBreed = "Tabby";
 		String expectedHairColor = "Orange";
 		int expectedAge = 7;
+		String expectedID = "NB-14-324";
+		boolean expectedIsFixed = true;
 		GregorianCalendar expectedArrivalDate = new GregorianCalendar(2011, 0, 11);
 		GregorianCalendar expectedDepartureDate = new GregorianCalendar(2013, 4, 12);
-		Cat cat = new Cat(expectedName, expectedAge, expectedGender, expectedBreed, expectedHairColor, expectedArrivalDate, expectedDepartureDate);
+		
+		Cat cat = new Cat(expectedName, expectedAge, expectedGender, expectedBreed, expectedHairColor, expectedID, expectedIsFixed, expectedArrivalDate, expectedDepartureDate);
+		
 		assertSame(expectedName, cat.getName());
 		assertSame(expectedAge, cat.getAge());
 		assertSame(expectedGender, cat.getGender());
 		assertSame(expectedBreed, cat.getBreed());
 		assertSame(expectedHairColor, cat.getHairColor());
+		assertSame(expectedID, cat.getID());
+		assertSame(expectedIsFixed, cat.isFixed());
 		assertSame(expectedArrivalDate, cat.getArrivalDate());
 		assertSame(expectedDepartureDate, cat.getExpectedDepartureDate());
 	}
