27444bfbb159d2c860a3ead7a80d690a9ef9718e
NATE DUBETZ <nbdubetz@gmail.com>
2014-10-26 21:31:54 -0400

    Update to main view

--- NewBeginnings/src/mainWindow/MainView.java
+++ NewBeginnings/src/mainWindow/MainView.java
@@ -5,16 +5,18 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JRadioButtonMenuItem;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
-public class MainView{
+public class MainView {
 
 	private JButton addNewCatButton;
 	private JFrame frame;
@@ -34,7 +36,6 @@ public class MainView{
 		this.frame = new JFrame("New Beginnings");
 		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		
 		addComponentsToPane();
 
 		this.frame.setSize(1000, 700);
@@ -45,21 +46,21 @@ public class MainView{
 
 	public void addComponentsToPane() {
 		this.panelToContainThemAll = new JPanel();
-		
+
 		this.panelToContainThemAll.setLayout(new BorderLayout());
 
 		final JMenuBar menuBar = createTheMenuBar();
 
 		final JPanel topPanel = createTopPanel();
-		
+
 		this.hostPanel = makeBottomPanel();
-		
+
 		setPanelToScrollPane(this.hostPanel);
-		
+
 		this.frame.setJMenuBar(menuBar);
-		
+
 		this.panelToContainThemAll.add(topPanel, BorderLayout.PAGE_START);
-		this.frame.getContentPane().add(panelToContainThemAll);
+		this.frame.getContentPane().add(this.panelToContainThemAll);
 	}
 
 	private JMenuBar createTheMenuBar() {
@@ -68,45 +69,50 @@ public class MainView{
 		final JMenu viewModeMenu = new JMenu("View Mode");
 		menuBar.add(fileMenu);
 		menuBar.add(viewModeMenu);
-		menuBar.setBackground(new Color(252, 245, 235));	
-		 exitAction = new JMenuItem("Exit");
-		 formAction = new JMenuItem("Forms View");
-		 dataBaseAction = new JMenuItem("DataBase View");
-		
-		viewModeMenu.add(formAction);
-		viewModeMenu.add(dataBaseAction);
-		fileMenu.add(exitAction);
-		
+		menuBar.setBackground(new Color(252, 245, 235));
+		this.exitAction = new JMenuItem("Exit");
+
+		ButtonGroup group = new ButtonGroup();
+		this.formAction = new JRadioButtonMenuItem("Forms View");
+		this.dataBaseAction = new JRadioButtonMenuItem("DataBase View");
+		this.dataBaseAction.setSelected(true);
+
+		group.add(this.formAction);
+		group.add(this.dataBaseAction);
+		viewModeMenu.add(this.formAction);
+		viewModeMenu.add(this.dataBaseAction);
+		fileMenu.add(this.exitAction);
+
 		return menuBar;
 	}
 
 	private JPanel createTopPanel() {
 		JPanel topPanel = new JPanel();
 		final JPanel searchPanel = createSearchPanel();
-		
+
 		topPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 300, 20));
 		topPanel.setPreferredSize(new Dimension(1000, 100));
 		topPanel.setBackground(new Color(47, 140, 171));
 		this.addNewCatButton = new JButton("Add New Cat");
-		
+
 		topPanel.add(searchPanel);
 		topPanel.add(this.addNewCatButton);
-		
+
 		return topPanel;
 	}
 
 	private JPanel createSearchPanel() {
 		JPanel searchPanel = new JPanel();
 		final JTextField searchBar = new JTextField("Search For A Cat");
-		
+
 		final JButton searchButton = new JButton("Search");
-		
+
 		searchPanel.setBackground(new Color(47, 140, 171));
-	
+
 		searchBar.setPreferredSize(new Dimension(150, 20));
 		searchPanel.add(searchBar);
 		searchPanel.add(searchButton);
-		
+
 		return searchPanel;
 	}
 
@@ -118,16 +124,16 @@ public class MainView{
 	public void changePanelOnScrollPane(JPanel bottomPanel) {
 		this.previousPanel = this.hostPanel;
 		this.hostPanel = bottomPanel;
-		
+
 		this.panelToContainThemAll.remove(this.scrollPane);
-		
+
 		setPanelToScrollPane(this.hostPanel);
 		this.panelToContainThemAll.validate();
 	}
 
 	private JPanel makeBottomPanel() {
 		JPanel bottomPanel = new JPanel();
-		
+
 		bottomPanel.setBackground(new Color(201, 226, 233));
 
 		return bottomPanel;
@@ -139,15 +145,15 @@ public class MainView{
 		this.dataBaseAction.addActionListener(controller);
 		this.exitAction.addActionListener(controller);
 		this.formAction.addActionListener(controller);
-		
+
 	}
-	
-	public void buildDataBaseView(){
+
+	public void buildDataBaseView() {
 		this.frame.remove(this.panelToContainThemAll);
 		this.addComponentsToPane();
 		this.frame.validate();
 	}
-	
+
 	public void buildFormsView(JPanel mainPanel) {
 		this.frame.remove(this.panelToContainThemAll);
 		this.panelToContainThemAll = mainPanel;
@@ -166,18 +172,17 @@ public class MainView{
 	protected JButton getAddNewCatButton() {
 		return this.addNewCatButton;
 	}
-	
+
 	public JMenuItem getExitActionMenuItem() {
-		return exitAction;
-	}
-	
-	public JMenuItem getFormActionMenuItem() {
-		return formAction;
-	}
-	
-	public JMenuItem getDataBaseActionMenuItem() {
-		return dataBaseAction;
+		return this.exitAction;
 	}
 
-	
+	public JMenuItem getFormActionMenuItem() {
+		return this.formAction;
+	}
+
+	public JMenuItem getDataBaseActionMenuItem() {
+		return this.dataBaseAction;
+	}
+
 }
