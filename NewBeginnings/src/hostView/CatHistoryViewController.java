8f17be9ea2da88c373e176506d3ec30c967ec028
NATE DUBETZ <nbdubetz@gmail.com>
2014-11-30 21:16:48 -0500

    Last one
    Should be properly merged now.

--- NewBeginnings/src/hostView/CatHistoryViewController.java
+++ NewBeginnings/src/hostView/CatHistoryViewController.java
@@ -2,4 +2,17 @@ package hostView;
 
 public class CatHistoryViewController {
 
+	private final AnimalInfoViewController animalInfoViewController;
+	private CatHistoryView catHistoryView = null;
+
+	public CatHistoryViewController(
+			AnimalInfoViewController animalInfoViewController) {
+		this.animalInfoViewController = animalInfoViewController;
+	}
+
+	public CatHistoryView buildView() {
+		this.catHistoryView = new CatHistoryView(null);
+		return this.catHistoryView;
+	}
+
 }
